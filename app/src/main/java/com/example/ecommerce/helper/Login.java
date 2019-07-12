fd01751d2d4488e8ab71b279b4d8306a8ae3397b
vaibhavdangayachvd <vaibhavdangayachvd@gmail.com>
2019-07-12 09:53:59 +0530

    Implemented User Session with server side validation using observer

--- app/src/main/java/com/example/ecommerce/helper/Login.java
+++
@@ -1,8 +0,0 @@
-package com.example.ecommerce.helper;
-
-import android.content.Context;
-
-public interface Login {
-    public boolean tryLogin(String username,String password);
-    public boolean isLoggedIn();
-}

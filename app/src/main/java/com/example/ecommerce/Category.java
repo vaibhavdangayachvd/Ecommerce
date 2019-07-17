417663059a89fe530c8f3e8becb4c394552cc886
santanu58342 <santa.banerjee102@gmail.com>
2019-07-17 22:24:02 +0530

    Layouts for category,product and cart

---
+++ app/src/main/java/com/example/ecommerce/Category.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class Category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+        //make it fragment and place the logic that it will information from server
+    }
+}
---
+++ app/src/main/java/com/example/ecommerce/Product_List.java
@@ -0,0 +1,12 @@
+package com.example.ecommerce;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class Product_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+    }
+}
